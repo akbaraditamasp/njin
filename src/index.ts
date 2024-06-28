b640778788ddec2bc3292777b0066f40717f097d
akbaraditamasp <akbaraditamasp@gmail.com>
2024-06-29 05:47:50 +0700

    feat: index

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import middleware from "./middleware";
+import plugin from "./plugin";
+import server from "./server";
+
+const njin = {
+  middleware,
+  plugin,
+  server,
+};
+
+export default njin;
